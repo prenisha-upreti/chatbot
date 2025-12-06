d687d026557085013d78c77a83dc8a2c2b0dab50
Prenisha Upreti <upretipre@gmail.com>
2025-12-06 22:33:08 +0545

    url changed

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 4002;
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://chatbott-wine.vercel.app/" // <-- your frontend domain here
+  origin: "https://chatbott-wine.vercel.app/", // <-- your frontend domain here
+    methods: ['GET', 'POST', 'OPTIONS'],
 }));
 
 async function startServer() {
